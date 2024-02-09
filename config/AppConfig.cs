7d79469b4d618252105b645290e41f142c80fb2e
William <will27528@gmail.com>
2024-02-09 18:57:03 +0000

    Start work on app stuff, update checking

---
+++ config/AppConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Config {
+    public class AppConfig {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Channel { get; set; }
+        public Dictionary<string, Updates> UpdateChannel { get; set; }
+        public class Updates {
+            public string Endpoint { get; set; }
+        }
+        
+    }
+}
---
+++ launcher/MainScript.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+namespace Launcher {
+	public partial class MainScript : Control
+	{
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			string[] args = OS.GetCmdlineArgs();
+		
+		}
+		
+	}
+}
